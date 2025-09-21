ee3cfbccf9b8d7503057f02e5a42df2feda008f6
Adam Killam <adammkillam@gmail.com>
2025-09-21 11:16:01 -0230

    Clarify operator documentation

--- OpenLanguage/WordprocessingML/Operators/Comparison.cs
+++ OpenLanguage/WordprocessingML/Operators/Comparison.cs
@@ -2,17 +2,37 @@ using System;
 
 namespace OpenLanguage.WordprocessingML.Operators
 {
+    public static class OperatorConstants
+    {
+        public const sbyte OperatorMask = 0b111;
+    }
+
     /// <summary>
     /// Enumeration of comparison operators.
+    ///
+    /// This can be seen in practice as what would be a bitpacked union in C.
+    /// First bit represents "Equal": whether the two values can be equal.
+    /// Second bit represents "LessThan": whether the first value can be less than the second
+    /// Third bit represents "GreaterThan": whether the first value can be greater than the second.
+    /// Combining these bits allows representation of all standard comparison operators.
+    /// For example:
+    ///   - Equal (0b001): only equal
+    ///   - LessThan (0b010): only less than
+    ///   - LessThanOrEqual (0b011): less than or equal
+    ///   - GreaterThan (0b100): only greater than
+    ///   - GreaterThanOrEqual (0b101): greater than or equal
+    ///   - NotEqual (0b110): less than or greater than (not equal)
+    ///   - All (0b111): equal, less than, or greater than (always true)
+    ///   - None (0b000): no relation (always false)
     /// </summary>
     public enum ComparisonOperator : sbyte
     {
-        Equal = 0b01,
-        NotEqual = ~ComparisonOperator.Equal,
-        LessThan = 0b10,
-        LessThanOrEqual = ComparisonOperator.Equal | ComparisonOperator.LessThan,
-        GreaterThan = ~ComparisonOperator.LessThanOrEqual,
-        GreaterThanOrEqual = ~ComparisonOperator.LessThan,
+        Equal = 0b001,
+        LessThan = 0b010,
+        LessThanOrEqual = ComparisonOperator.Equal | ComparisonOperator.LessThan, // 0b011
+        GreaterThan = ~ComparisonOperator.LessThanOrEqual & OperatorConstants.OperatorMask, // 0b010
+        GreaterThanOrEqual = ~ComparisonOperator.LessThan & OperatorConstants.OperatorMask, // 0b101
+        NotEqual = ComparisonOperator.LessThan | ComparisonOperator.GreaterThan, // 0b110
     }
 
     public static class ComparisonOperatorExtensions
